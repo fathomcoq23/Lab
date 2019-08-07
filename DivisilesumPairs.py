f014ab5ad5327cc578d03e89fd9096fcca3c886c
Fabrice lionel <fabrice_nganhou@yahoo.fr>
2019-08-07 13:24:19 +0200

    Create DivisilesumPairs.py

---
+++ DivisilesumPairs.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the divisibleSumPairs function below.
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(len(ar)):
+        for j in range(len(ar)):
+            if i<j:
+                total = ar[i] + ar[j]
+                if total % k == 0:
+                    count+= 1
+                else:
+                    pass
+              
+    return(count)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
