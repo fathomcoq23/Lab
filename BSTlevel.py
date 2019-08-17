21ae8d709a329ef898d5e92c9dbe5fca9c4871ac
Fabrice lionel <fabrice_nganhou@yahoo.fr>
2019-08-17 14:23:35 +0200

    Create BSTlevel.py

---
+++ BSTlevel.py
@@ -0,0 +1,36 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        #Write your code here
+        queue = [root]  #list of elements in the tree
+        for current in queue: #access every element in queue
+            if current: #if true is retruned
+                print(current.data, end=" ")    #print current data
+                queue.append(current.left)  #print left
+                queue.append(current.right) #print right
+            
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
