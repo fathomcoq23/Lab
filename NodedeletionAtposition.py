eb98eedea6ee1d104e6344b9acc70fe7b0cf12a7
Fabrice lionel <fabrice_nganhou@yahoo.fr>
2019-08-25 14:03:11 +0200

    Create NodedeletionAtposition.py

---
+++ NodedeletionAtposition.py
@@ -0,0 +1,86 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the deleteNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def deleteNode(head, position):
+    count = 0
+    curNode = head.next
+    predNode = head
+    while curNode is not None:
+        if count+1 == position:
+            tail = curNode.next
+            predNode.next = tail
+        elif count == position == 0:
+            tail = head.next
+            head = tail
+        else:
+            pass
+        count += 1
+        curNode = curNode.next
+        predNode = predNode.next
+    return head
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
